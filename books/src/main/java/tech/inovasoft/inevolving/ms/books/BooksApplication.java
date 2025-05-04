c2e59f8bdadfef0e854daa1a9ade2d832a2ab382
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-05-04 10:10:39 -0300

    feat: Commit Inicial

---
+++ books/src/main/java/tech/inovasoft/inevolving/ms/books/BooksApplication.java
@@ -0,0 +1,13 @@
+package tech.inovasoft.inevolving.ms.books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksApplication.class, args);
+	}
+
+}
