5a2ffb89543ad3f2d084435f40006a39a1317812
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-06-06 17:54:04 -0300

    Desenvolvendo teste do end-point, addBook

--- books/src/test/java/tech/inovasoft/inevolving/ms/books/api/BooksControllerTest.java
+++ books/src/test/java/tech/inovasoft/inevolving/ms/books/api/BooksControllerTest.java
@@ -1,10 +1,21 @@
 package tech.inovasoft.inevolving.ms.books.api;
 
+import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import tech.inovasoft.inevolving.ms.books.domain.dto.request.RequestBookDTO;
+import tech.inovasoft.inevolving.ms.books.domain.model.Status;
+
+import java.util.UUID;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -13,9 +24,38 @@ public class BooksControllerTest {
     @LocalServerPort
     private int port;
 
+    UUID idUser = UUID.randomUUID();
+
     @Test
     public void addBook_ok() {
-        //TODO: Desenvolver teste do End-Point
+        RequestBookDTO requestBookDTO = new RequestBookDTO(
+                "Title",
+                "Author",
+                "Theme",
+                "https://inovasoft.tech/wp-content/webp-express/webp-images/uploads/2025/03/LogoTipoBorda-512x319px.png.webp"
+        );
+
+        // Cria a especificação da requisição
+        RequestSpecification requestSpecification = given()
+                .contentType(ContentType.JSON);
+
+        // Faz a requisição GET e armazena a resposta
+        ValidatableResponse response = requestSpecification
+                .body(requestBookDTO)
+                .when()
+                .post("http://localhost:" + port + "/ms/books/" + idUser)
+                .then();
+
+
+        // Valida a resposta
+        response.assertThat().statusCode(200).and()
+                .body("id", Matchers.notNullValue()).and()
+                .body("idUser", equalTo(idUser.toString())).and()
+                .body("title", equalTo(requestBookDTO.title())).and()
+                .body("author", equalTo(requestBookDTO.author())).and()
+                .body("theme", equalTo(requestBookDTO.theme())).and()
+                .body("status", equalTo(Status.TO_DO))
+                .body("coverImage", equalTo(requestBookDTO.coverImage()));
     }
 
     @Test
